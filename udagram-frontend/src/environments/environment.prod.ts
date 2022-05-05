bde208d3c8508b415deb9851ced9c983aafd02a5
Mayar-Hassanin98 <mayar.hassanin9814@gmail.com>
2022-05-05 18:49:16 +0200

    deploy App

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "udagram-api",
+  apiHost: "udagram-api-dev.eba-9kbsy8nv.us-east-1.elasticbeanstalk.com",
 };
 
 /*
