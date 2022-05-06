d98d4391cacde43f47d3aa21500174e6817709c4
Mayar-Hassanin98 <mayar.hassanin9814@gmail.com>
2022-05-07 01:20:20 +0200

    update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,11 +1,9 @@
 eb init udagram-api --region us-east-1
 eb use udagram-api-dev
-eb setenv DB_PORT=$DB_PORT
-eb setenv PORT=$PORT
+eb setenv PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv POSTGRES_DB=$POSTGRES_DB
 eb setenv POSTGRES_HOST=$POSTGRES_HOST
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv RDS_DIALECT=$RDS_DIALECT
 eb setenv JWT_SECRET=$JWT_SECRET
 eb deploy udagram-api-dev
